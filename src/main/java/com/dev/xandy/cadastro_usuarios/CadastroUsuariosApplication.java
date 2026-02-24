d784aa185d3dde017d5713be24eb2948ee9e2360
João Artur Cruz <arturcruz842@gmail.com>
2026-02-24 20:41:18 -0300

    Commitando o novo controler.java

--- src/main/java/com/dev/xandy/cadastro_usuarios/CadastroUsuariosApplication.java
+++ src/main/java/com/dev/xandy/cadastro_usuarios/CadastroUsuariosApplication.java
@@ -8,6 +8,7 @@ public class CadastroUsuariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroUsuariosApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/dev/xandy/cadastro_usuarios/controler.java
@@ -0,0 +1,19 @@
+package com.dev.xandy.cadastro_usuarios;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class controler {
+
+    @GetMapping("/boasvindas")
+    public String boavindas() {
+        return "Boa vindas ao sistema de cadastro";
+    }
+
+}
+
